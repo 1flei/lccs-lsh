ef18c3ce256905e2d794a0b6f5cb70a2550d70c9
1flei <yfleiii@gmail.com>
2019-03-29 16:38:26 +0800

    initialize project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <boost/program_options.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace boost::program_options;
+
+
+int main()
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
